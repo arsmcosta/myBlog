c11c01139b613cdb647a073ac9b45f46818c4a17
Renato Costa <arsmcosta@gmail.com>
2021-09-21 21:08:43 -0300

    Listando posts na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   uid?: string;
@@ -24,35 +26,60 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home() {
+export default function Home({ results }: PostPagination) {
   return (
     <>
       <header>spacetravelling</header>
       <main className={styles.contentContainer}>
         <div className={styles.posts}>
-          <a>
-            <strong>Titulo do post</strong>
-            <span>Lorem ipsum dolor sit amet, consectetur</span>
-            <time>20 de setembro de 2021</time>
-          </a>
-
-          <a>
-            <strong>Titulo do post</strong>
-            <span>Lorem ipsum dolor sit amet, consectetur</span>
-            <div className={styles.info}>
-            <time>20 de setembro de 2021</time>
-            <span>Autor</span>
-            </div>
-          </a>
+          <ul>
+            {results.map(post => (
+              <main key={post.uid} className={styles.contentContainer}>
+                <div className={styles.posts}>
+                {console.log(post.data)}
+                  <a>
+                    <strong>{post.data.title}</strong>
+                    <span>{post.data.author}</span>
+                    <time>{post.first_publication_date}</time>
+                  </a>
+                </div>
+              </main>
+            ))}
+          </ul>
         </div>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.content'],
+      pageSize: 100,
+    }
+  );
 
-//   // TODO
-// };
+  const results = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.first_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: {
+        title: post.data.title,
+      },
+    };
+  });
+  return {
+    props: {
+      results,
+    },
+  };
+};
